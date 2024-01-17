da4f43595920cf4ac6b1480e0a015277b6f5600d
exaul <137360873+exaul@users.noreply.github.com>
2024-01-16 19:08:26 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,55 @@
+﻿using ApiMusic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMusic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> _songs = new List<Song> {
+
+              new Song
+            {
+                Id = 1,
+                TiTle = "Kids with Guns",
+                Language = "English"
+            },
+            new Song
+            {
+                Id = 2,
+                TiTle = "Karma",
+                Language = "English"
+            },
+            new Song
+            {
+                Id = 3,
+                TiTle = "Iglesia",
+                Language = "Español"
+            },
+
+            new Song
+            {
+                Id = 4,
+                TiTle = "мой мармеладный",
+                Language = "Ruso"
+            }
+
+        };
+
+        [HttpGet]//Mediante este controlador podemos hacer la petición
+        public IEnumerable<Song> GetAllSongs()
+        {
+            return _songs;
+        }
+
+        [HttpPost]
+        public IActionResult SaveSong([FromBody] Song newSong) 
+        {
+            _songs.Add(newSong);
+            return Ok();
+        }
+
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiMusic.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string TiTle { get; set; }
+
+        public string Language { get; set; }
+
+
+    }
+}
